f726495468012e890c536eca854b4966acfbe136
Ivan Admaers <admaers@icloud.com>
2022-03-23 11:48:05 +0400

    add new conctants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,6 @@
+import path from 'path';
+
 export const defaultBase = 'EUR';
+export const RATES_CACHE_FILE = 'rates.json';
+export const RATES_CACHE_PATH = path.resolve(`./src/cache/${RATES_CACHE_FILE}`);
+export const RATES_CACHE_UPDATE_TIME_IN_MINUTES = 60;
