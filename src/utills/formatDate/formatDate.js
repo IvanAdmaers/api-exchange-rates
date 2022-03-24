ca693f1afab73c0fd117a7d3a559c0dda7be3e34
Ivan Admaers <admaers@icloud.com>
2022-03-24 13:26:29 +0400

    create a function to formats dates

---
+++ src/utills/formatDate/formatDate.js
@@ -0,0 +1,25 @@
+/**
+ * This function formats a date in YYYY-MM-DD format
+ * 
+ * @param {string} date - Date
+ * @returns {string} Formatted date
+ */
+const formatDate = (date) => {
+  const currentDate = new Date(date);
+
+  let month = (currentDate.getMonth() + 1).toString();
+  let day = date.getDate().toString();
+  const year = currentDate.getFullYear();
+
+  if (month.length < 2) {
+    month = `0${month}`;
+  }
+
+  if (day.length < 2) {
+    day = `0${day}`;
+  }
+
+  return [year, month, day].join('-');
+};
+
+export default formatDate;
---
+++ src/utills/formatDate/index.js
@@ -0,0 +1,3 @@
+import formatDate from './formatDate';
+
+export default formatDate;
