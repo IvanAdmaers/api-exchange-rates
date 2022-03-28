1e4846f69b76326e66333419ce6f38f0364a9e2b
Ivan Admaers <admaers@icloud.com>
2022-03-28 18:49:01 +0400

    migration to ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 
-export const defaultBase = 'EUR';
-export const RATES_CACHE_FILE = 'rates.json';
-export const RATES_CACHE_PATH = path.resolve(`./src/cache/${RATES_CACHE_FILE}`);
-export const RATES_CACHE_UPDATE_TIME_IN_MINUTES = 60;
+export const defaultBase: string = 'EUR';
+export const RATES_CACHE_FILE: string = 'rates.json';
+export const RATES_CACHE_PATH: string = path.resolve(
+  `./src/cache/${RATES_CACHE_FILE}`
+);
+export const RATES_CACHE_UPDATE_TIME_IN_MINUTES: number = 60;
