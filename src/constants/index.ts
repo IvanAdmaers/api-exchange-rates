4b98a2827e67c71b9de23889cbc1ebd858c13b82
Ivan Admaers <admaers@icloud.com>
2022-03-29 14:45:32 +0400

    change extencion to .js

--- src/constants/index.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-
-export const DEFAULT_BASE: string = 'USD';
-export const RATES_CACHE_FILE: string = 'rates.json';
-export const RATES_CACHE_PATH: string = path.resolve(
-  `./src/cache/${RATES_CACHE_FILE}`
-);
-export const RATES_CACHE_UPDATE_TIME_IN_MINUTES: number = 60;
