067b7fd08f1ace4dcfde1cb3b3bb5fd39d3a63be
Ivan Admaers <admaers@icloud.com>
2022-03-29 10:12:06 +0400

    refactoring for new default base

--- src/helpers/updateRates/updateRates.js
+++ src/helpers/updateRates/updateRates.js
@@ -1,13 +1,31 @@
+import cache from 'memory-cache-pro';
+
 import BankAPIService from '../../services/BankAPIService';
 
 import { FileSystem } from '../../libs';
-import { RATES_CACHE_PATH } from '../../constants';
+import { modifyWithBase } from '../../utills';
+import {
+  RATES_CACHE_PATH,
+  DEFAULT_BASE,
+  LATEST_RATES_DATE_CACHE_KEY,
+} from '../../constants';
 
 /**
  * This function update rates
  */
 const updateRates = async () => {
-  const rates = await BankAPIService.historical();
+  const ratesList = await BankAPIService.historical();
+  const ratesBase = 'EUR';
+
+  const rates = {};
+
+  Object.entries(ratesList).forEach(([key, value], index) => {
+    if (index === 0) {
+      cache.put(LATEST_RATES_DATE_CACHE_KEY, key);
+    }
+
+    rates[key] = modifyWithBase(DEFAULT_BASE, { ...value, [ratesBase]: 1 });
+  });
 
   const ratesJSON = JSON.stringify(rates);
 
