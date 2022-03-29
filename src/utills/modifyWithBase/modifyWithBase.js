480d29a11121879aa6953d9017627267befc3729
Ivan Admaers <admaers@icloud.com>
2022-03-29 10:12:42 +0400

    remove import of default base

--- src/utills/modifyWithBase/modifyWithBase.js
+++ src/utills/modifyWithBase/modifyWithBase.js
@@ -1,5 +1,3 @@
-import { DEFAULT_BASE } from '../../constants';
-
 /**
  * This function modifies rates depending on currency
  *
@@ -12,7 +10,7 @@ const modifyWithBase = (base = '', data = {}) => {
   const currentBase = data[base];
 
   if (!currentBase) {
-    return { ...data, [DEFAULT_BASE]: 1 };
+    return { ...data };
   }
 
   Object.entries(data).forEach(([key, value]) => {
@@ -24,8 +22,6 @@ const modifyWithBase = (base = '', data = {}) => {
 
     result[key] = value / currentBase;
   });
-
-  result[DEFAULT_BASE] = 1 / currentBase;
 
   return result;
 };
