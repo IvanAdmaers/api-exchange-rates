e13d6d55d8536599f7d7af00cf6a16bdb508043b
Ivan Admaers <admaers@icloud.com>
2022-03-29 10:09:18 +0400

    change default base

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-export const DEFAULT_BASE: string = 'EUR';
+export const DEFAULT_BASE: string = 'USD';
 export const RATES_CACHE_FILE: string = 'rates.json';
 export const RATES_CACHE_PATH: string = path.resolve(
   `./src/cache/${RATES_CACHE_FILE}`
