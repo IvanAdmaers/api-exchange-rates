d55d9706e7c89b761fcd2f962ad7a6d8be9f1843
Ivan Admaers <admaers@icloud.com>
2022-03-30 11:38:48 +0400

    update imports for getHistoricalRates

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,5 +2,12 @@ import shouldUpdateRates from './shouldUpdateRates';
 import setRates from './setRates';
 import modifyRates from './modifyRates';
 import getLatestRates from './getLatestRates';
+import getHistoricalRates from './getHistoricalRates';
 
-export { shouldUpdateRates, setRates, modifyRates, getLatestRates };
+export {
+  shouldUpdateRates,
+  setRates,
+  modifyRates,
+  getLatestRates,
+  getHistoricalRates,
+};
