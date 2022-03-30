b156474e25866336eab6f30d5c7bb62fc8981fbf
Ivan Admaers <admaers@icloud.com>
2022-03-30 11:31:37 +0400

    update imports for modifyRates

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import shouldUpdateRates from './shouldUpdateRates';
 import setRates from './setRates';
+import modifyRates from './modifyRates';
 
-export { shouldUpdateRates, setRates };
+export { shouldUpdateRates, setRates, modifyRates };
