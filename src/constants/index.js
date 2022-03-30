d70848fe85d98774b638ec0c9a17a22b8867d207
Ivan Admaers <admaers@icloud.com>
2022-03-30 09:37:01 +0400

    renaming a contstant

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,12 +4,12 @@ const DEFAULT_BASE = 'USD';
 const RATES_CACHE_FILE = 'rates.json';
 const RATES_CACHE_PATH = path.resolve(`./src/cache/${RATES_CACHE_FILE}`);
 const RATES_CACHE_UPDATE_TIME_IN_MINUTES = 60;
-const RATES_FAKE_VALUE = '{}';
+const RATES_PLUG = '{}';
 
 module.exports = {
   DEFAULT_BASE,
   RATES_CACHE_FILE,
   RATES_CACHE_PATH,
   RATES_CACHE_UPDATE_TIME_IN_MINUTES,
-  RATES_FAKE_VALUE,
+  RATES_PLUG,
 };
