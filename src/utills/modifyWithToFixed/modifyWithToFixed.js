370d14fafbaf19214a5c65405c9f7871208938e7
Ivan Admaers <admaers@icloud.com>
2022-04-01 12:14:00 +0400

    cancel to use a default value

--- src/utills/modifyWithToFixed/modifyWithToFixed.js
+++ src/utills/modifyWithToFixed/modifyWithToFixed.js
@@ -4,10 +4,10 @@ import { toFixed } from '..';
  * This function modifies rates value by digits
  *
  * @param {Object} rates - Rates
- * @param {number} [digits=6] - Digits
+ * @param {number} digits - Digits
  * @returns {Object} - Modified rates
  */
-const modifyWithToFixed = (rates, digits = 6) => {
+const modifyWithToFixed = (rates, digits) => {
   const result = {};
 
   Object.entries(rates).forEach(([rateKey, rateValue]) => {
