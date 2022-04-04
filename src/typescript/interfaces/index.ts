5fd7d6044f8a438f8a31faf0d243d6a1665fd375
Ivan Admaers <admaers@icloud.com>
2022-04-04 11:14:17 +0400

    move types to interfaces

---
+++ src/typescript/interfaces/index.ts
@@ -0,0 +1,7 @@
+export interface RatesInterface {
+  [key: string]: number;
+}
+
+export interface RatesListInterface {
+  [key: string]: RatesInterface;
+}
--- src/typescript/types/index.ts
+++
@@ -1,2 +0,0 @@
-export type ratesType = { [key: string]: number };
-export type ratesListType = { [key: string]: { [key: string]: number } };
