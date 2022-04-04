976bb9cd808066bdc288b065b988249fdb6ee312
Ivan Admaers <admaers@icloud.com>
2022-04-04 12:31:22 +0400

    add a test case

--- src/utills/sortObjectAlphabetically/sortObjectAlphabetically.test.js
+++ src/utills/sortObjectAlphabetically/sortObjectAlphabetically.test.js
@@ -7,4 +7,11 @@ describe('sortObjectAlphabetically', () => {
 
     expect(sortObjectAlphabetically(object)).toEqual(correctly);
   });
+
+  it('should return a correct result when some of keys are a number', () => {
+    const object = { 4: 'number', c: 3, b: 2, a: 1 };
+    const correctly = { 4: 'number', a: 1, b: 2, c: 3 };
+
+    expect(sortObjectAlphabetically(object)).toEqual(correctly);
+  });
 });
