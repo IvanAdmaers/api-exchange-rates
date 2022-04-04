92d577e10bea7a05b180974be279ef88e2577c6a
Ivan Admaers <admaers@icloud.com>
2022-04-04 13:50:16 +0400

    migration to typescript

--- src/utills/formatDate/formatDate.ts
+++ src/utills/formatDate/formatDate.ts
@@ -1,12 +1,12 @@
 /**
  * This function formats a date in YYYY-MM-DD format
  */
-const formatDate = (date) => {
-  const currentDate = new Date(date);
+const formatDate = (date: string) => {
+  const currentDate: Date = new Date(date);
 
-  let month = (currentDate.getMonth() + 1).toString();
-  let day = currentDate.getDate().toString();
-  const year = currentDate.getFullYear();
+  let month: string = (currentDate.getMonth() + 1).toString();
+  let day: string = currentDate.getDate().toString();
+  const year: number = currentDate.getFullYear();
 
   if (month.length < 2) {
     month = `0${month}`;
