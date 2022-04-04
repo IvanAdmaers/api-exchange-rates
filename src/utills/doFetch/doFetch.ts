a4700ac88e1713249befcbd07523846a0b0c5682
Ivan Admaers <admaers@icloud.com>
2022-04-04 13:57:38 +0400

    add a type for the first argument

--- src/utills/doFetch/doFetch.ts
+++ src/utills/doFetch/doFetch.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-const doFetch = async (url = '') => {
+const doFetch = async (url: string) => {
   const req = await fetch(url);
   const res = await req.text();
 
