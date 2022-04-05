cbd171a880008d139847d0d1ab7f0ca3d9ce3e53
Ivan Admaers <admaers@icloud.com>
2022-04-05 15:24:57 +0400

    refactoring for dates

--- src/controllers/exchangeRatesController.js
+++ src/controllers/exchangeRatesController.js
@@ -3,12 +3,14 @@ import { modifyRates, getRatesList, defineEndpoint } from '../helpers';
 import { rates as ratesList, lastRatesDate } from '../cache/rates.json';
 
 export const rates = async (req, res) => {
-  const { base, symbols, amount, start_date, end_date } = req.query;
-  const { date: reqDate } = req.params;
-
-  const startDate = start_date ? new Date(start_date) : null;
-  const endDate = start_date ? new Date(end_date) : null;
-  const date = reqDate ? new Date(reqDate) : null;
+  const {
+    base,
+    symbols,
+    amount,
+    start_date: startDate,
+    end_date: endDate,
+  } = req.query;
+  const { date } = req.params;
 
   const endpoint = defineEndpoint({ date, startDate, endDate });
   const isTimeseries = endpoint === 'timeseries';
