ffa9b4de6a374c7913c091ef31f3aa5e15d9d326
Ivan Admaers <admaers@icloud.com>
2022-04-05 15:43:30 +0400

    add tests

---
+++ src/helpers/getRatesList/getRatesList.test.ts
@@ -0,0 +1,61 @@
+import { Endpoint } from '../../typescript/objects';
+import getRatesList from '.';
+
+const { Latest, Historical, Timeseries } = Endpoint;
+
+const date1 = '2022-03-30';
+const date2 = '2022-03-31';
+const date3 = '2022-04-01';
+
+const rates = {
+  [date1]: {
+    AUD: 1.331,
+    CAD: 1.248,
+    EUR: 0.898,
+    GBP: 0.76,
+    USD: 1,
+  },
+  [date2]: {
+    AUD: 1.335,
+    CAD: 1.251,
+    EUR: 0.9,
+    GBP: 0.762,
+    USD: 1,
+  },
+  [date3]: {
+    AUD: 1.329,
+    CAD: 1.249,
+    EUR: 0.904,
+    GBP: 0.761,
+    USD: 1,
+  },
+};
+
+describe('getRatesList', () => {
+  it(`should return a correct result for the ${Latest} endpoint`, () => {
+    const date = date1;
+
+    expect(
+      getRatesList({ rates, endpoint: Latest, lastRatesDate: date })
+    ).toEqual(rates[date]);
+  });
+
+  it(`should return a correct result for the ${Historical} endpoint`, () => {
+    const date = date2;
+
+    expect(getRatesList({ rates, endpoint: Historical, date })).toEqual(
+      rates[date]
+    );
+  });
+
+  it(`should return a correct result for the ${Timeseries} endpoint`, () => {
+    expect(
+      getRatesList({
+        rates,
+        endpoint: Timeseries,
+        startDate: date2,
+        endDate: date3,
+      })
+    ).toEqual({ [date2]: { ...rates[date2] }, [date3]: { ...rates[date3] } });
+  });
+});
