60ecead515fcf835a45955785f15e69a7657cccf
Ivan Admaers <admaers@icloud.com>
2022-04-05 15:43:37 +0400

    add types

--- src/helpers/getRatesList/getRatesList.ts
+++ src/helpers/getRatesList/getRatesList.ts
@@ -1,8 +1,13 @@
+import { RatesListInterface } from '../../typescript/interfaces';
+import { Endpoint } from '../../typescript/objects';
+
 import getLatestRates from '../getLatestRates';
 import getHistoricalRates from '../getHistoricalRates';
 import getTimeseriesRates from '../getTimeseriesRates';
 
 import { formatDate } from '../../utills';
+
+const { Latest, Historical, Timeseries } = Endpoint;
 
 /**
  * This function gets rates list
@@ -14,22 +19,41 @@ const getRatesList = ({
   date,
   startDate,
   endDate,
+}: {
+  rates: RatesListInterface;
+  lastRatesDate?: string;
+  endpoint: string;
+  date?: string;
+  startDate?: string;
+  endDate?: string;
 }) => {
   switch (endpoint) {
-    case 'latest': {
+    case Latest: {
+      if (!lastRatesDate) {
+        throw new Error('lastRatesDate is not set');
+      }
+
       const formattedLastRatesDate = formatDate(lastRatesDate);
 
       return getLatestRates(rates, formattedLastRatesDate);
     }
 
-    case 'historical': {
+    case Historical: {
+      if (!date) {
+        throw new Error('date is not set');
+      }
+
       const formattedDate = formatDate(date);
 
       return getHistoricalRates(rates, formattedDate);
     }
 
-    case 'timeseries':
-      return getTimeseriesRates(rates, startDate, endDate);
+    case Timeseries:
+      if (!startDate || !endDate) {
+        throw new Error('startDate or endDate is not set');
+      }
+
+      return getTimeseriesRates(rates, new Date(startDate), new Date(endDate));
 
     default:
       throw new Error('Endpoint is unknown');
