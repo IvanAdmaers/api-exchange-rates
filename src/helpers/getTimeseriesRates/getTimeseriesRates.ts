3c22433322536143fd3850d7045b8d8c670cf149
Ivan Admaers <admaers@icloud.com>
2022-04-06 09:41:29 +0400

    migration to typescript

--- src/helpers/getTimeseriesRates/getTimeseriesRates.ts
+++ src/helpers/getTimeseriesRates/getTimeseriesRates.ts
@@ -1,16 +1,26 @@
+import {
+  RatesListInterface,
+  RatesInterface,
+} from '../../typescript/interfaces';
 import { getDatesInRange, formatDate } from '../../utills';
 
 /**
  * This function gets timeseries rates
  */
-const getTimeseriesRates = (rates, startDate, endDate) => {
-  const result = {};
+const getTimeseriesRates = (
+  rates: RatesListInterface,
+  startDate: Date,
+  endDate: Date
+): RatesListInterface => {
+  const result: RatesListInterface = {};
 
-  const dates = getDatesInRange(startDate, endDate);
-  const dateList = dates.map((itemDate) => formatDate(itemDate));
+  const dates: Array<Date> = getDatesInRange(startDate, endDate);
+  const dateList: Array<string> = dates.map((itemDate) =>
+    formatDate(itemDate.toDateString())
+  );
 
-  dateList.forEach((itemDate) => {
-    const dateData = rates[itemDate];
+  dateList.forEach((itemDate: string) => {
+    const dateData: RatesInterface = rates[itemDate];
 
     if (!dateData) {
       return;
