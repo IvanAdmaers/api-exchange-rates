d21f6919e60a9cd1472d3fb44b29819c8c65ee38
Ivan Admaers <admaers@icloud.com>
2022-04-07 14:40:45 +0400

    add a constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 const DEFAULT_BASE = 'USD';
 const RATES_CACHE_FILE = 'rates.json';
-const RATES_CACHE_PATH = path.resolve(`./src/cache/${RATES_CACHE_FILE}`);
+const RATES_CACHE_PATH_TO_FOLDER = path.resolve(`./src/cache`);
+const RATES_CACHE_PATH = path.resolve(`${RATES_CACHE_PATH_TO_FOLDER}/${RATES_CACHE_FILE}`);
 const RATES_CACHE_UPDATE_TIME_IN_MINUTES = 60;
 const RATES_PLUG = '{}';
 const TO_FIXED_DEFAULT_VALUE = 6;
@@ -10,6 +11,7 @@ const TO_FIXED_DEFAULT_VALUE = 6;
 module.exports = {
   DEFAULT_BASE,
   RATES_CACHE_FILE,
+  RATES_CACHE_PATH_TO_FOLDER,
   RATES_CACHE_PATH,
   RATES_CACHE_UPDATE_TIME_IN_MINUTES,
   RATES_PLUG,
