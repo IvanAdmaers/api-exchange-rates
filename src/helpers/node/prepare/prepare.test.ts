e106391fc1ea988c21feeea1f1e23c14b5158cf1
Ivan Admaers <admaers@icloud.com>
2022-04-09 09:56:54 +0400

    add tests

---
+++ src/helpers/node/prepare/prepare.test.ts
@@ -0,0 +1,27 @@
+import { fs, vol } from 'memfs';
+import prepare from '.';
+import { FileSystem } from '../../../libs';
+import {
+  RATES_CACHE_PATH_TO_FOLDER,
+  RATES_CACHE_PATH,
+  RATES_PLUG,
+} from '../../../constants';
+
+vol.mkdirSync(RATES_CACHE_PATH_TO_FOLDER, { recursive: true });
+
+jest.mock('fs/promises', () => fs.promises);
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('prepare', () => {
+  it('should create a plug rates file', async () => {
+    await prepare();
+
+    const file = await FileSystem.readFile(RATES_CACHE_PATH);
+    const fileContent = file.toString();
+
+    expect(fileContent).toBe(RATES_PLUG);
+  });
+});
