ef678a038b0998f1fb39b133239b8eb3b31a258b
Ivan Admaers <admaers@icloud.com>
2022-04-13 14:19:43 +0400

    add tests

--- src/helpers/setRates/setRates.test.ts
+++ src/helpers/setRates/setRates.test.ts
@@ -1,17 +1,21 @@
 import memory from 'memory-cache-pro';
+import { fs as memFS, vol } from 'memfs';
 import {
   RatesListInterface,
   RatesInterface,
 } from '../../typescript/interfaces';
-import { BankAPIService } from '../../services';
+import { FileSystem } from '../../libs';
 import { modifyWithBase, modifyWithToFixed } from '../../utills';
 import setRates from '.';
+import { getRates } from '..';
 import {
   DEFAULT_BASE,
   TO_FIXED_DEFAULT_VALUE,
   RATES_MEMORY_KEY,
   LAST_RATES_DATE_MEMORY_KEY,
   RATES_CACHE_UPDATE_TIME_IN_MINUTES,
+  RATES_CACHE_PATH_TO_FOLDER,
+  RATES_CACHE_PATH,
 } from '../../constants';
 
 const cacheTimeInMilliseconds: number =
@@ -23,22 +27,24 @@ const rates: RatesListInterface = {
 };
 
 jest
-  .spyOn(BankAPIService, 'historical')
+  .spyOn(jest.requireActual('..'), 'getRates')
   .mockReturnValue(Promise.resolve(rates));
 
+jest.mock('fs/promises', () => memFS.promises);
+
 describe('setRates', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
+    await vol.promises.mkdir(RATES_CACHE_PATH_TO_FOLDER, { recursive: true });
     jest.useFakeTimers();
   });
 
   afterEach(() => {
     jest.clearAllTimers();
+    vol.reset();
   });
 
-  it('should put rates and a last rates date to the memory', async () => {
-    await setRates();
-
-    const expectRates: RatesListInterface = {};
+  const getModifiedRates = (): RatesListInterface => {
+    const result: RatesListInterface = {};
 
     Object.entries(rates).forEach(([key, value]: [string, RatesInterface]) => {
       const modifiedWithBase: RatesInterface = modifyWithBase(
@@ -50,8 +56,16 @@ describe('setRates', () => {
         TO_FIXED_DEFAULT_VALUE
       );
 
-      expectRates[key] = modifiedWithToFixed;
+      result[key] = modifiedWithToFixed;
     });
+
+    return result;
+  };
+
+  const expectRates: RatesListInterface = getModifiedRates();
+
+  it('should put rates and a last rates date to the memory', async () => {
+    await setRates();
 
     expect(memory.get(RATES_MEMORY_KEY)).toEqual(expectRates);
     expect(memory.get(LAST_RATES_DATE_MEMORY_KEY)).toEqual(date);
@@ -79,5 +93,33 @@ describe('setRates', () => {
     jest.advanceTimersByTime(cacheTimeInMilliseconds - 1);
 
     expect(onRatesExpiredCallbackMock).toBeCalledTimes(0);
+  });
+
+  it('should call getRates with `setRatesFromCache` = false', async () => {
+    await setRates();
+    expect(getRates).toHaveBeenCalledWith({ setRatesFromCache: false });
+  });
+
+  it('should call getRates with `setRatesFromCache` = true', async () => {
+    await setRates({ setRatesFromCache: true });
+    expect(getRates).toHaveBeenCalledWith({ setRatesFromCache: true });
+  });
+
+  it('should do cache of rates when `doCacheRates` = true', async () => {
+    await setRates({ doCacheRates: true });
+
+    const ratesCacheBuffer = await FileSystem.readFile(RATES_CACHE_PATH);
+    const ratesCacheJSON = ratesCacheBuffer.toString();
+    const ratesCache = JSON.parse(ratesCacheJSON);
+
+    expect(ratesCache).toEqual(expectRates);
+  });
+
+  it('should not do cache of rates when `doCacheRates` = false', async () => {
+    await setRates({ doCacheRates: false });
+
+    const fileExists = await FileSystem.fileExists(RATES_CACHE_PATH);
+
+    expect(fileExists).toBe(false);
   });
 });
