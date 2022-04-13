82e75653fe6b43bb4baafe9b732c09979187a4fd
Ivan Admaers <admaers@icloud.com>
2022-04-13 13:40:34 +0400

    refactoring due to moving from filesystem to memory

--- src/helpers/setRates/setRates.ts
+++ src/helpers/setRates/setRates.ts
@@ -1,9 +1,11 @@
-import cache from 'memory-cache-pro';
+import memory from 'memory-cache-pro';
 import {
   RatesListInterface,
   RatesInterface,
 } from '../../typescript/interfaces';
 import { BankAPIService } from '../../services';
+import { FileSystem } from '../../libs';
+import { getRates } from '..';
 import { modifyWithBase, modifyWithToFixed } from '../../utills';
 import {
   DEFAULT_BASE,
@@ -11,6 +13,7 @@ import {
   RATES_MEMORY_KEY,
   LAST_RATES_DATE_MEMORY_KEY,
   RATES_CACHE_UPDATE_TIME_IN_MINUTES,
+  RATES_CACHE_PATH,
 } from '../../constants';
 
 /**
@@ -18,11 +21,15 @@ import {
  */
 const setRates = async ({
   onRatesExpiredCallback = setRates,
+  doCacheRates,
+  setRatesFromCache,
 }: {
   onRatesExpiredCallback?: Function;
+  doCacheRates?: boolean;
+  setRatesFromCache?: boolean;
 } = {}): Promise<void> => {
   // TODO: add the type `RatesListInterface` below
-  const ratesList: object = await BankAPIService.historical();
+  const ratesList = await getRates({ setRatesFromCache });
   const ratesBase: string = BankAPIService.getBase();
 
   const rates: RatesListInterface = {};
@@ -50,13 +57,17 @@ const setRates = async ({
 
   const cacheTimeInMilliseconds = RATES_CACHE_UPDATE_TIME_IN_MINUTES * 60_000;
 
-  cache.put(
+  if (doCacheRates) {
+    await FileSystem.writeFile(RATES_CACHE_PATH, JSON.stringify(rates));
+  }
+
+  memory.put(
     RATES_MEMORY_KEY,
     rates,
     cacheTimeInMilliseconds,
     onRatesExpiredCallback
   );
-  cache.put(LAST_RATES_DATE_MEMORY_KEY, lastRatesDate);
+  memory.put(LAST_RATES_DATE_MEMORY_KEY, lastRatesDate);
 };
 
 export default setRates;
