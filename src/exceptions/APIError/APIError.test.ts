f1745f2bd3f3f89ed095cb35a3a9b8ce6e7f8641
Ivan Admaers <admaers@icloud.com>
2022-04-14 10:48:09 +0400

    change Error to lowercase

--- src/exceptions/APIError/APIError.test.ts
+++ src/exceptions/APIError/APIError.test.ts
@@ -2,29 +2,29 @@ import APIError from '.';
 
 describe('APIError', () => {
   it('should return the error object from the static class', () => {
-    const Error = APIError.notFound();
+    const error = APIError.notFound();
 
-    expect(Error instanceof APIError).toBe(true);
-    expect(typeof Error.code).toBe('number');
-    expect(typeof Error.message).toBe('string');
+    expect(error instanceof APIError).toBe(true);
+    expect(typeof error.code).toBe('number');
+    expect(typeof error.message).toBe('string');
   });
 
   it('should return the error object from the static class with a custom message', () => {
     const text = 'Not found custom text';
-    const Error = APIError.notFound(text);
+    const error = APIError.notFound(text);
 
-    expect(Error instanceof APIError).toBe(true);
-    expect(typeof Error.code).toBe('number');
-    expect(Error.message).toBe(text);
+    expect(error instanceof APIError).toBe(true);
+    expect(typeof error.code).toBe('number');
+    expect(error.message).toBe(text);
   });
 
   it('should return an error object', () => {
     const code = 404;
     const message = 'Page not found';
 
-    const Error = new APIError(code, message);
+    const error = new APIError(code, message);
 
-    expect(Error.code).toBe(code);
-    expect(Error.message).toBe(message);
+    expect(error.code).toBe(code);
+    expect(error.message).toBe(message);
   });
 });
