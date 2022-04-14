22c7a2c746bb4e1e545213916d2ff6fdee2c4fb1
Ivan Admaers <admaers@icloud.com>
2022-04-14 09:15:39 +0400

    migration to typescript

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,25 +1,12 @@
-const path = require('path');
+import path from 'path';
 
-const DEFAULT_BASE = 'USD';
-const RATES_CACHE_FILE = 'rates.json';
-const RATES_CACHE_PATH_TO_FOLDER = path.resolve(`./src/cache`);
-const RATES_CACHE_PATH = path.resolve(
+export const DEFAULT_BASE: string = 'USD';
+export const RATES_CACHE_FILE: string = 'rates.json';
+export const RATES_CACHE_PATH_TO_FOLDER: string = path.resolve(`./src/cache`);
+export const RATES_CACHE_PATH: string = path.resolve(
   `${RATES_CACHE_PATH_TO_FOLDER}/${RATES_CACHE_FILE}`
 );
-const RATES_CACHE_UPDATE_TIME_IN_MINUTES = 60;
-const RATES_PLUG = '{}';
-const TO_FIXED_DEFAULT_VALUE = 6;
-const RATES_MEMORY_KEY = 'rates';
-const LAST_RATES_DATE_MEMORY_KEY = 'lastRatesDate';
-
-module.exports = {
-  DEFAULT_BASE,
-  RATES_CACHE_FILE,
-  RATES_CACHE_PATH_TO_FOLDER,
-  RATES_CACHE_PATH,
-  RATES_CACHE_UPDATE_TIME_IN_MINUTES,
-  RATES_PLUG,
-  TO_FIXED_DEFAULT_VALUE,
-  RATES_MEMORY_KEY,
-  LAST_RATES_DATE_MEMORY_KEY,
-};
+export const RATES_CACHE_UPDATE_TIME_IN_MINUTES: number = 60;
+export const TO_FIXED_DEFAULT_VALUE: number = 6;
+export const RATES_MEMORY_KEY: string = 'rates';
+export const LAST_RATES_DATE_MEMORY_KEY: string = 'lastRatesDate';
