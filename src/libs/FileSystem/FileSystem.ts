a2f90a02642c67207ad4c24dd4426d7f503283c8
Ivan Admaers <admaers@icloud.com>
2022-04-20 11:25:41 +0400

    migration to typescript

--- src/libs/FileSystem/FileSystem.ts
+++ src/libs/FileSystem/FileSystem.ts
@@ -1,7 +1,10 @@
 import fs from 'fs/promises';
 
 class FileSystem {
-  async fileExists(path) {
+  /**
+   * This method checks a file exists
+   */
+  public async fileExists(path: string): Promise<boolean> {
     try {
       await fs.access(path);
 
@@ -11,19 +14,28 @@ class FileSystem {
     }
   }
 
-  async readFile(path) {
+  /**
+   * This method reads a file
+   */
+  public async readFile(path: string): Promise<Buffer> {
     const file = await fs.readFile(path);
 
     return file;
   }
 
-  async getFileMeta(path) {
+  /**
+   * This method gets a file meta
+   */
+  public async getFileMeta(path: string) {
     const meta = await fs.stat(path);
 
     return meta;
   }
 
-  async writeFile(path, data) {
+  /**
+   * This method writes data to a file
+   */
+  public async writeFile(path: string, data: string) {
     await fs.writeFile(path, data);
 
     return true;
@@ -31,13 +43,8 @@ class FileSystem {
 
   /**
    * Change the file system timestamps of the object referenced by path
-   *
-   * @param {string} path - Path to file
-   * @param {Date} atime - Atime
-   * @param {Date} mtime - Mtime
-   * @returns {<Promise>undefined}
    */
-  async utimes(path, atime, mtime) {
+  public async utimes(path: string, atime: Date, mtime: Date): Promise<void> {
     await fs.utimes(path, atime, mtime);
   }
 }
