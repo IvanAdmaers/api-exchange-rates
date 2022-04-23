ad1c7a1a83b6d5df5810d2ab5de37705102b1437
Ivan Admaers <admaers@icloud.com>
2022-04-23 10:14:34 +0400

    refactoring of the returning status code

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -4,7 +4,9 @@ import { getErrorBody } from '../utills';
 
 const errorHandlerMiddleware = () => (error, req, res, next) => {
   if (error instanceof APIError) {
-    return res.status(error.code).json(getErrorBody(error.code, error.message));
+    const status = error.code < 200 ? 200 : error.code;
+
+    return res.status(status).json(getErrorBody(error.code, error.message));
   }
 
   if (error instanceof SyntaxError && error.status === 400 && 'body' in error) {
