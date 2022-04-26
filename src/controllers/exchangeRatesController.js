ffbd7620fc8681def2a56461a62c0e1f3e5ef4ce
Ivan Admaers <admaers@icloud.com>
2022-04-26 14:07:14 +0400

    add handling of errors

--- src/controllers/exchangeRatesController.js
+++ src/controllers/exchangeRatesController.js
@@ -17,49 +17,58 @@ const ratesList = memory.get(RATES_MEMORY_KEY);
 const lastRatesDate = memory.get(LAST_RATES_DATE_MEMORY_KEY);
 
 export const rates = async (req, res, next) => {
-  const { symbols, start_date: startDate, end_date: endDate } = req.query;
-  const base = req.query.base ?? DEFAULT_BASE;
-  const amount = +req.query.amount;
+  try {
+    const { symbols, start_date: startDate, end_date: endDate } = req.query;
+    const base = req.query.base ?? DEFAULT_BASE;
+    const amount = +req.query.amount;
 
-  const { date } = req.params;
+    const { date } = req.params;
 
-  const endpoint = defineEndpoint({ date, startDate, endDate });
-  const isTimeseries = endpoint === 'timeseries';
+    const endpoint = defineEndpoint({ date, startDate, endDate });
+    const isTimeseries = endpoint === 'timeseries';
 
-  const currentRatesList = getRatesList({
-    rates: ratesList,
-    lastRatesDate,
-    endpoint,
-    date,
-    startDate,
-    endDate,
-  });
+    const currentRatesList = getRatesList({
+      rates: ratesList,
+      lastRatesDate,
+      endpoint,
+      date,
+      startDate,
+      endDate,
+    });
 
-  if (!currentRatesList) {
-    return next(APIError.noResult());
+    // TODO: refactor this
+    if (currentRatesList instanceof APIError) {
+      return next(currentRatesList);
+    }
+
+    if (!currentRatesList) {
+      return next(APIError.noResult());
+    }
+
+    const ratesData = modifyRates({
+      rates: currentRatesList,
+      base,
+      symbols,
+      amount,
+      isTimeseries,
+    });
+
+    const requestInfo = getRequestInfo({
+      endpoint,
+      lastRatesDate,
+      date,
+      startDate,
+      endDate,
+      symbols,
+      amount,
+      base,
+    });
+
+    return res.json({
+      ...requestInfo,
+      rates: ratesData,
+    });
+  } catch (error) {
+    return next(error);
   }
-
-  const ratesData = modifyRates({
-    rates: currentRatesList,
-    base,
-    symbols,
-    amount,
-    isTimeseries,
-  });
-
-  const requestInfo = getRequestInfo({
-    endpoint,
-    lastRatesDate,
-    date,
-    startDate,
-    endDate,
-    symbols,
-    amount,
-    base,
-  });
-
-  return res.json({
-    ...requestInfo,
-    rates: ratesData,
-  });
 };
