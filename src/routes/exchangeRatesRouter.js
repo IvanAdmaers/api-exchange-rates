41b698b137e782417db0a61f51b63d2545ffab63
Ivan Admaers <admaers@icloud.com>
2022-04-30 10:52:51 +0400

    integrate a middleware for caching

--- src/routes/exchangeRatesRouter.js
+++ src/routes/exchangeRatesRouter.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { rates } from '../controllers/exchangeRatesController';
+import cacheMiddleware from '../middlewares/cacheMiddleware';
 
 const router = Router();
 
-router.get('/ecb/latest', rates);
-router.get('/ecb/:date', rates);
-router.get('/ecb/timeseries', rates);
+const cacheTime = 15 * 1000 * 60;
+
+router.get('/ecb/latest', cacheMiddleware(cacheTime), rates);
+router.get('/ecb/:date', cacheMiddleware(cacheTime), rates);
+router.get('/ecb/timeseries', cacheMiddleware(cacheTime), rates);
 
 export default router;
