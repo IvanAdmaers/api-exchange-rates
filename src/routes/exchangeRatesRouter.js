65cdcc6335d9177f01d170cc2cf8cfe2786e4ba5
Ivan Admaers <admaers@icloud.com>
2022-04-30 12:03:38 +0400

    add a validator

--- src/routes/exchangeRatesRouter.js
+++ src/routes/exchangeRatesRouter.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 import { rates } from '../controllers/exchangeRatesController';
 import cacheMiddleware from '../middlewares/cacheMiddleware';
+import validatorMiddleware from '../middlewares/validatorMiddleware';
 
 const router = Router();
 
 const cacheTime = 15 * 1000 * 60;
+
+router.use(validatorMiddleware());
 
 router.get('/ecb/latest', cacheMiddleware(cacheTime), rates);
 router.get('/ecb/:date', cacheMiddleware(cacheTime), rates);
