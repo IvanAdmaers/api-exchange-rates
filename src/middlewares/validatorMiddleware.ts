6fa4f0a9f67ce70d1ba75f51577ab54f10978b5b
Ivan Admaers <admaers@icloud.com>
2022-05-02 12:17:05 +0400

    add a validation for timeseries endpoint

--- src/middlewares/validatorMiddleware.ts
+++ src/middlewares/validatorMiddleware.ts
@@ -30,6 +30,46 @@ const validatorMiddleware =
       }
     }
 
+    const { start_date: startDateQuery, end_date: endDateQuery } = req.query;
+
+    if (startDateQuery || endDateQuery) {
+      if (!startDateQuery) {
+        return next(APIError.invalidStartDate());
+      }
+
+      if (!endDateQuery) {
+        return next(APIError.invalidEndDate());
+      }
+
+      if (!isValidDateFormat(startDateQuery as string)) {
+        return next(APIError.invalidStartDate());
+      }
+
+      if (!isValidDateFormat(endDateQuery as string)) {
+        return next(APIError.invalidEndDate());
+      }
+
+      const startDate: Date = new Date(startDateQuery as string);
+      const endDate: Date = new Date(endDateQuery as string);
+
+      const invalidDateString: string = 'Invalid Date';
+
+      if (startDate.toString() === invalidDateString) {
+        return next(APIError.invalidStartDate());
+      }
+
+      if (endDate.toString() === invalidDateString) {
+        return next(APIError.invalidEndDate());
+      }
+
+      if (startDate.getFullYear() > endDate.getFullYear()) {
+        return next(APIError.invalidStartDate());
+      }
+
+      // TODO: to think about add a validator of days length bettween two dates
+      // that now in getRatesList
+    }
+
     return next();
   };
 
