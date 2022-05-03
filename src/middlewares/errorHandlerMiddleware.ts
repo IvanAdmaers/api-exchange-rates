0591718a65bdb9c1f9c6226d9e03d8e86fc71385
Ivan Admaers <admaers@icloud.com>
2022-05-03 14:17:21 +0400

    migration to typescript

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,21 +1,41 @@
-import { APIError } from '../exceptions';
+import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
 
+import { APIError } from '../exceptions';
 import { getErrorBody } from '../utills';
 
-const errorHandlerMiddleware = () => (error, req, res, next) => {
-  if (error instanceof APIError) {
-    return res.status(200).json(getErrorBody(error.code, error.message));
-  }
+interface SyntaxErrorInterface extends SyntaxError {
+  status?: number;
+}
 
-  if (error instanceof SyntaxError && error.status === 400 && 'body' in error) {
+const errorHandlerMiddleware =
+  () =>
+  (
+    error: ErrorRequestHandler | Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (error instanceof APIError) {
+      return res.status(200).json(getErrorBody(error.code, error.message));
+    }
+
+    const possibleSyntaxError = error as SyntaxErrorInterface;
+
+    if (
+      error instanceof SyntaxError &&
+      possibleSyntaxError.status === 400 &&
+      'body' in error
+    ) {
+      return res
+        .status(possibleSyntaxError.status)
+        .json(getErrorBody(possibleSyntaxError.status, 'Incorrect request'));
+    }
+
+    console.error(error);
+
     return res
-      .status(error.status)
-      .json(getErrorBody(error.status, 'Incorrect request'));
-  }
-
-  console.error(error);
-
-  return res.status(500).json(getErrorBody(500, 'Something went really wrong'));
-};
+      .status(500)
+      .json(getErrorBody(500, 'Something went really wrong'));
+  };
 
 export default errorHandlerMiddleware;
