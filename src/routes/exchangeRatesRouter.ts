42b33946a7c543476ba9f27f3fade11c23e02b1c
Ivan Admaers <admaers@icloud.com>
2022-05-13 09:21:26 +0400

    allow cors requests

--- src/routes/exchangeRatesRouter.ts
+++ src/routes/exchangeRatesRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import cors from 'cors';
 
 import { rates } from '../controllers/exchangeRatesController';
 import cacheMiddleware from '../middlewares/cacheMiddleware';
 import validatorMiddleware from '../middlewares/validatorMiddleware';
 
 const router = Router();
+
+router.use(cors());
 
 const cacheTime: number = 15 * 1000 * 60;
 
